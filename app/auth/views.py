2ba3f4d8cc17256ee15675563669944eaef666f7
rly <375833274@qq.com>
2017-11-29 22:44:01 +0800

    improve fork update

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
 from flask import g, render_template, redirect, request, url_for, flash, current_app
 from flask_login import login_user, logout_user, login_required, current_user
 
@@ -46,6 +47,7 @@ def github_login(access_token):
              permission=Permission.GITHUB_USER).save()
         flash('Login with Github successfully!')
     User.objects(username=_github_username).update(github_access_token=access_token)
+    User.objects(username=_github_username).update(last_seen=datetime.utcnow())
     _user = User.objects(username=_github_username).first()
     login_user(_user, True)
     
