2f84edb42e6ac0e542bdf45702d11c44592c403b
rly <375833274@qq.com>
2017-12-19 00:21:39 +0800

    update readme

--- config.py
+++ config.py
@@ -15,8 +15,6 @@ class Config:
     # Overview page config.
     SHOW_NUMBER_FOR_PAGE = 6 # Project number per page in index page.
 
-    # API_CALL_RETRY_TIMES = 5
-
     # Github Oauth Apps config.
     GITHUB_CLIENT_ID = os.environ.get('GITHUB_CLIENT_ID')
     GITHUB_CLIENT_SECRET = os.environ.get('GITHUB_CLIENT_SECRET')
@@ -25,12 +23,12 @@ class Config:
     SECRET_KEY = os.environ.get('INFOX_SECRET_KEY')
 
     # E-mail Config
-    MAIL_SERVER = 'smtp.126.com'
+    MAIL_SERVER = 'smtp.126.com' # Change it to your mail server
     MAIL_PORT = 465
     MAIL_USERNAME = os.environ.get('INFOX_MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('INFOX_MAIL_PASSWORD')
     MAIL_USE_SSL = True
-    FLASK_MAIL_SENDER = '<infox_help@126.com>'
+    FLASK_MAIL_SENDER = '<infox_help@126.com>' # Change it to your mail sender
     FLASK_MAIL_SUBJECT_PREFIX = '[INFOX(Forks-Insight)]'
 
 
