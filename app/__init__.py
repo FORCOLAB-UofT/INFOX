f1dc265f7096dee9f91e90e0bb7f244baad66a8e
rly <375833274@qq.com>
2018-01-21 18:48:29 +0800

    use celery to separate cralwer

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_mongoengine import MongoEngine
 from flask_login import LoginManager
 from flask_github import GitHub
 from flask_mail import Mail
+from flask_celery import Celery
+# from celery import Celery
 
 from config import config
 
@@ -13,7 +15,9 @@ db = MongoEngine()
 mail = Mail()
 github = GitHub()
 login_manager = LoginManager()
-# celery = Celery()
+# celery = Celery(__name__, broker=Config.CELERY_BROKER_URL)
+celery = Celery()
+
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
@@ -47,8 +51,9 @@ def create_app(config_name):
     mail.init_app(app)
     github.init_app(app)
     login_manager.init_app(app)
-    # celery.init_app(app)
-    
+    # celery.conf.update(app.config)
+    celery.init_app(app)
+
     if app.config.get('SSL_DISABLE', None):
         from flask_sslify import flask_SSLify
         sslify = SSLify(app)
---
+++ celery_worker.py
@@ -0,0 +1,4 @@
+from app import celery, create_app
+
+app = create_app('default')
+app.app_context().push()
