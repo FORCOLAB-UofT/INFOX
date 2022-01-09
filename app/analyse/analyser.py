c251b88015b86a7e31a445d63dc464d9a846ac37
Cansin Varol <cansinvarol@me.com>
2022-01-08 19:47:29 -0500

    fixed bug where new searched repos wouldn't show forks

--- app/analyse/analyser.py
+++ app/analyse/analyser.py
@@ -48,6 +48,35 @@ def send_mail_for_repo_finish(project_name):
                     username=user.username,
                 )
 
+def get_active_forks(repo, access_token):
+    active_forks = []
+    result_length = 100
+    page = 1
+
+    while result_length == 100:
+        request_url = "https://api.github.com/repos/%s/forks?per_page=100&page=%d" % (
+            repo,
+            page,
+        )
+
+        res = requests.get(
+            url=request_url,
+            headers={
+                "Accept": "application/json",
+                "Authorization": "token {}".format(access_token),
+            },
+        )
+
+        forks = res.json()
+        result_length = len(forks)
+
+        for fork in forks:
+            if fork["pushed_at"] > fork["created_at"]:
+                active_forks.append(fork)
+
+        page += 1
+
+    return active_forks
 
 @celery.task
 def start_analyse(repo, access_token):
@@ -97,7 +126,8 @@ def start_analyse(repo, access_token):
             },
         )
         repo_forks_list = res.json()
-        localfile_tool.write_to_file(forks_list_path, repo_forks_list)
+        active_forks = get_active_forks(repo,access_token )
+        localfile_tool.write_to_file(forks_list_path, active_forks)
 
     print("finish fetch fork list for %s" % repo)
 
@@ -188,3 +218,205 @@ def check_repo(repo, access_token):
     except:
         return None
     return result
+
+
+# import time
+# import os
+# import json
+# import requests
+# from datetime import datetime
+
+# from flask import current_app, render_template
+# from flask_github import GitHub
+# from . import project_updater
+# from .util import localfile_tool
+# from ..models import *
+# from .. import celery
+# from flask_mail import Message
+# from .. import mail
+
+
+# def send_mail(to, subject, template, **kwargs):
+#     """use async to send email
+#     :param to
+#     :param subject
+#     :param template
+#     :param kwargs
+#     """
+#     msg = Message(
+#         current_app.config["FLASK_MAIL_SUBJECT_PREFIX"] + subject,
+#         sender=current_app.config["FLASK_MAIL_SENDER"],
+#         recipients=[to],
+#     )
+#     msg.html = render_template(template, **kwargs)
+#     mail.send(msg)
+
+
+# def send_mail_for_repo_finish(project_name):
+#     _user_list = User.objects(followed_projects=project_name)
+#     for user in _user_list:
+#         if user.email is not None:
+#             last_email = user.repo_email_time.get(project_name, None)
+#             new_dict = user.repo_email_time
+#             new_dict[project_name] = datetime.utcnow()
+#             User.objects(username=user.username).update_one(repo_email_time=new_dict)
+#             if last_email is None:
+#                 # Only send email when first add.
+#                 send_mail(
+#                     user.email,
+#                     "Repo Status Update",
+#                     "email.html",
+#                     project_name=project_name,
+#                     username=user.username,
+#                 )
+
+# def get_active_forks(repo, access_token):
+#     active_forks = []
+#     result_length = 100
+#     page = 1
+
+#     while result_length == 100:
+#         request_url = "https://api.github.com/repos/%s/forks?per_page=100&page=%d" % (
+#             repo,
+#             page,
+#         )
+
+#         res = requests.get(
+#             url=request_url,
+#             headers={
+#                 "Accept": "application/json",
+#                 "Authorization": "token {}".format(access_token),
+#             },
+#         )
+
+#         forks = res.json()
+#         result_length = len(forks)
+
+#         for fork in forks:
+#             if fork["pushed_at"] > fork["created_at"]:
+#                 active_forks.append(fork)
+
+#         page += 1
+
+#     return active_forks
+
+# @celery.task
+# def start_analyse(repo, access_token):
+#     """Start analyse on repo using github_api_caller(contains personal access token)
+#     Args:
+#         app context, repo, github_api_caller
+#     Returns:
+#         None
+#     """
+#     app = current_app._get_current_object()
+#     github_api_caller = GitHub(app)
+
+#     @github_api_caller.access_token_getter
+#     def token_getter():
+#         return access_token
+
+#     print("-----start analysing for %s-----" % repo)
+
+#     repo_info = github_api_caller.get("repos/%s" % repo)
+#     print("finish fetch repo info for %s" % repo)
+
+#     # Save forks' list into local
+#     forks_list_path = (
+#         current_app.config["LOCAL_DATA_PATH"] + "/" + repo + "/forks_list.json"
+#     )
+#     if current_app.config["USE_LOCAL_FORKS_LIST"] and os.path.exists(forks_list_path):
+#         with open(forks_list_path) as read_file:
+#             repo_forks_list = json.load(read_file)
+#     else:
+#         # repo_forks_list = github_api_caller.get("repos/%s/forks?per_page=100" % repo)
+#         repo_forks_list = get_active_forks(repo,access_token )
+#         localfile_tool.write_to_file(forks_list_path, repo_forks_list)
+
+#     print("finish fetch fork list for %s" % repo)
+
+#     project_updater.start_update(repo, repo_info, repo_forks_list)
+
+#     # TODO: Fix email sending functionality
+#     # temporarily commented out to get working on local - laith
+#     # send_mail_for_repo_finish(repo)
+
+#     print("-----finish analysing for %s-----" % repo)
+
+
+# @celery.task
+# def check_waiting_list(username):
+#     """Check username's waiting list to crawl more
+#     Args:
+#         app context, username
+#     Returns:
+#         None
+#     """
+#     user = User.objects(username=username).first()
+#     if user.is_crawling == 1:
+#         return
+#     User.objects(username=username).update_one(set__is_crawling=1)
+
+#     access_token = user.github_access_token
+#     app = current_app._get_current_object()
+
+#     while True:
+#         waiting_list = User.objects(username=username).first().repo_waiting_list
+#         if (waiting_list is None) or (len(waiting_list) == 0):
+#             break
+#         for repo in waiting_list:
+#             User.objects(username=username).update_one(pull__repo_waiting_list=repo)
+
+#             with app.app_context():
+#                 start_analyse.delay(repo, access_token)
+
+#             if not current_app.config["USE_LOCAL_FORKS_LIST"]:
+#                 wait_time = check_repo(repo, access_token)["forks"] * 1.5 / 30
+#                 time.sleep(wait_time)
+
+#             # thread = threading.Thread(target=start_analyse, args=[app, repo, github_api_caller])
+#             # thread.setDaemon(True)
+#             # thread.start()
+#             # thread.join(10 * 60) # wait for 10 mins
+
+#     User.objects(username=username).update_one(set__is_crawling=0)
+
+
+# def add_repos(username, repos):
+#     """User (username) add some repos."""
+#     User.objects(username=username).update_one(push_all__repo_waiting_list=repos)
+
+#     # First updata for quick view.
+#     access_token = User.objects(username=username).first().github_access_token
+#     for repo in repos:
+#         repo_info = check_repo(repo, access_token)
+#         if repo_info is not None:
+#             project_updater.project_init(repo, repo_info)
+
+#     app = current_app._get_current_object()
+#     with app.app_context():
+#         check_waiting_list.delay(username)
+
+#     # threading.Thread(target=check_waiting_list, args=[username]).start()
+#     return True
+
+
+# def check_repo(repo, access_token):
+#     """Check repo existence.
+#     Args:
+#         repo(full name), access_token
+#     Returns:
+#         None for not found,
+#         json result for found.
+#     """
+#     app = current_app._get_current_object()
+#     github_api_caller = GitHub(app)
+
+#     @github_api_caller.access_token_getter
+#     def token_getter():
+#         return access_token
+
+#     try:
+#         result = github_api_caller.get("repos/%s" % repo)
+#     except:
+#         return None
+#     return result
--- app/analyse/compare_changes_crawler.py
+++ app/analyse/compare_changes_crawler.py
@@ -6,46 +6,34 @@ from bs4 import BeautifulSoup
 
 from .util import language_tool
 
-def fetch_commit_list(project_full_name):
-    """
-    Args:
-        project_full_name: for example: 'NeilBetham/Smoothieware'
-    Return:
-        commit_list {
-                title,
-                link,
-            }
-    """
+def fetch_commit_list(repo, fork):
     commit_list = []
-
-    url = 'https://github.com/%s/compare' % project_full_name
+    # todo find correct url for different branches. Here we make the assumption we are comparing master branches
+    url = "https://github.com/%s/compare/master...%s:master" % (
+        repo,
+        fork,
+    )
     s = requests.Session()
-    s.mount('https://github.com', HTTPAdapter(max_retries=3))
-
+    s.mount("https://github.com", HTTPAdapter(max_retries=3))
     try:
         diff_page = s.get(url, timeout=120)
         if diff_page.status_code != requests.codes.ok:
-            raise Exception('error on fetch compare page on %s!' % project_full_name)
+            raise Exception("error on fetch compare page on %s!" % repo)
     except:
-        raise Exception('error on fetch compare page on %s!' % project_full_name)
-
-    diff_page_soup = BeautifulSoup(diff_page.content, 'html.parser')
-    for ch in diff_page_soup.find_all('div', {'class': 'commit-message'}):
-        commit = ch.find('code').find('a')
-
-        href = commit.get('href')
-        if 'https://' not in href:
-            href = 'https://github.com' + href
+        raise Exception("error on fetch compare page on %s!" % repo)
+    diff_page_soup = BeautifulSoup(diff_page.content, "html.parser")
+    for commit in diff_page_soup.find_all(
+        "a", {"class": "Link--primary text-bold js-navigation-open markdown-title"}
+    ):
+        href = commit.get("href")
+        if "https://" not in href:
+            href = "https://github.com" + href
         title = commit.text
-
-        commit_list.append({
-            "title":title,
-            "link":href
-            })
+        commit_list.append({"title": title, "link": href})
     return commit_list
 
 
-def fetch_diff_code(project_full_name):
+def fetch_diff_code(repo,fork):
     """
     Args:
         project_full_name: for example: 'NeilBetham/Smoothieware'
@@ -59,16 +47,19 @@ def fetch_diff_code(project_full_name):
         }
     """
     file_list = []
-    url = 'https://github.com/%s/compare' % project_full_name
+    url = "https://github.com/%s/compare/master...%s:master" % (
+        repo,
+        fork,
+    )
     # It will first jump to https://github.com/author/repo/compare/version...author:repo,
     # then fetch from https://github.com/author/repo/compare/version...author:repo.patch
     try:
         url = requests.get(url, timeout=120).url + '.diff'
         r = requests.get(url, timeout=120)
         if r.status_code != requests.codes.ok:
-            raise Exception('error on fetch compare page on %s!' % project_full_name)
+            raise Exception('error on fetch compare page on %s!' % repo)
     except:
-        raise Exception('error on fetch compare page on %s!' % project_full_name)
+        raise Exception('error on fetch compare page on %s!' % repo)
 
     diff_list = r.text.split('diff --git')
     for diff in diff_list[1:]:
@@ -110,10 +101,8 @@ def fetch_diff_code(project_full_name):
     return file_list
     
 
-def fetch_compare_page(project_full_name):
-    """Compare the fork with the main branch.
-    Args:
-        project_full_name: for example: 'NeilBetham/Smoothieware'
+def fetch_compare_page(repo_name, fork_name):
+    """Compare the fork with the main repo.
     Return:
         A dict contains following fields :
         compare_result {
@@ -121,15 +110,15 @@ def fetch_compare_page(project_full_name):
             total_changed_line_number,
             
     """
-    print('START fetch fork: ', project_full_name)
+    print('START fetch fork: ', fork_name)
     try:
-        commit_list = fetch_commit_list(project_full_name)
-        file_list = fetch_diff_code(project_full_name)
+        commit_list = fetch_commit_list(repo_name, fork_name)
+        file_list = fetch_diff_code(repo_name, fork_name)
     except:
-        print('FAILED on fetch fork: ', project_full_name)
+        print('FAILED on fetch fork: ', fork_name)
         return None
 
-    print('SUCCESS on fetch fork: ', project_full_name)
+    print('SUCCESS on fetch fork: ', fork_name)
     return {"file_list": file_list,
             "commit_list": commit_list}
 
