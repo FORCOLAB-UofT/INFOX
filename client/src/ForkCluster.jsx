6d1f47ee30d2a7f99fc11066fc3fb006a9760506
Laith Hanania <laithhanania@MAIMANG_7-50e7144561dfadd.localdomain>
2022-02-09 16:12:53 -0500

    added search functionality for cluster and loading msg

--- client/src/ForkCluster.jsx
+++ client/src/ForkCluster.jsx
@@ -1,20 +1,67 @@
 import React, { useState } from "react";
 import { getForkClustering } from "./repository";
 import { ResponsiveNetworkCanvas } from "@nivo/network";
-import { Box } from "@mui/material";
+import { Box, Button, TextField, Grid } from "@mui/material";
+import Loading from "./common/Loading";
 
 const ForkCluster = () => {
   const [data, setData] = useState(null);
   const [annotations, setAnnotations] = useState(null);
+  const [searchText, setSearchText] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const onClickSearch = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    const response = await getForkClustering(searchText);
+    setData(response.data);
+
+    let ann = [];
+    response.data.nodes.forEach((node) => {
+      if (node.height === 1) {
+        ann.push({
+          type: "circle",
+          match: {
+            id: node.id,
+          },
+          note: node.id,
+          noteX: 10,
+          noteY: 30,
+          offset: 2,
+          noteTextOffset: 3,
+        });
+      }
+    });
+    setAnnotations(ann);
+    setLoading(false);
+  };
 
   return (
-    <div>
-      <div>
+    <Box>
+      <Box>
+        <form onSubmit={onClickSearch}>
+          <Grid container alignItems="center" paddingBottom={1}>
+            <Grid item paddingRight={1}>
+              <TextField
+                variant="standard"
+                label="Repository"
+                value={searchText}
+                onChange={(event) => {
+                  setSearchText(event.target.value);
+                }}
+              />
+            </Grid>
+            <Grid item marginTop={1}>
+              <Button variant="contained" type="submit">
+                Search
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+
         <button
           onClick={async () => {
-            const response = await getForkClustering(
-              "freeCodeCamp/freeCodeCamp"
-            );
+            const response = await getForkClustering("home-assistant/core");
             console.log(response);
             setData(response.data);
 
@@ -28,9 +75,9 @@ const ForkCluster = () => {
                   },
                   note: node.id,
                   noteX: 10,
-                  noteY: 10,
+                  noteY: 30,
                   offset: 2,
-                  noteTextOffset: 1,
+                  noteTextOffset: 3,
                 });
               }
             });
@@ -39,9 +86,11 @@ const ForkCluster = () => {
         >
           here
         </button>
-      </div>
+      </Box>
 
-      {data ? (
+      {loading ? (
+        <Loading loadingMessage="Please wait. This may take a few minutes." />
+      ) : data ? (
         <div
           style={{
             height: "100vh",
@@ -53,8 +102,8 @@ const ForkCluster = () => {
             linkDistance={function (e) {
               return e.distance;
             }}
-            centeringStrength={0.8}
-            repulsivity={120}
+            centeringStrength={0.4}
+            repulsivity={300}
             iterations={260}
             nodeColor={function (e) {
               return e.color;
@@ -67,10 +116,11 @@ const ForkCluster = () => {
             linkThickness={function (n) {
               return 2 + 2 * n.target.data.height;
             }}
+            annotations={annotations}
           />
         </div>
       ) : null}
-    </div>
+    </Box>
   );
 };
 
--- client/src/common/Loading.jsx
+++ client/src/common/Loading.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 // TODO: Make it appear only if it will take more than 0.5 seconds to load
-const Loading = () => {
+const Loading = ({ loadingMessage }) => {
   return (
     <Grid
       container
@@ -13,8 +15,17 @@ const Loading = () => {
       style={{ minHeight: "100%" }}
     >
       <CircularProgress />
+      <Box>
+        {loadingMessage ? (
+          <Typography variant="h6">{loadingMessage}</Typography>
+        ) : null}
+      </Box>
     </Grid>
   );
+};
+
+Loading.defaultProps = {
+  loadingMessage: null,
 };
 
 export default Loading;
