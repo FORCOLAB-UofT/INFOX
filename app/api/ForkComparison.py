dbc266235b9e092fda16992bf4bf24306bd7a4b1
John Perry <johnperry220@outlook.com>
2022-02-10 18:08:10 -0500

    Completed backend function to find common keywords between selected forks

--- app/api/ForkComparison.py
+++ app/api/ForkComparison.py
@@ -5,7 +5,9 @@ import json
 from flask import request
 import requests
 from ..models import User
-
+from ..analyse.compare_changes_crawler import fetch_commit_list
+from ..analyse.analyser import get_active_forks
+from rake_nltk import Rake
 
 class ForkComparison(Resource):
     def __init__(self, jwt):
@@ -13,5 +15,29 @@ class ForkComparison(Resource):
 
     @jwt_required()
     def get(self):
-        # asdf
-        pass
+
+        current_user = get_jwt_identity()
+        _user = User.objects(username=current_user).first()
+        
+        req_data = request.get_json()
+        forkNames = req_data.get("forks")
+        repoName = req_data.get("repo")
+
+        # a dictionary where each key (fork name) contains all commit messages of that fork  
+        forkCommits = dict()
+
+        # a list of all commits for each fork to be analyzed by Rake
+        forkSentences = []
+
+        # active_forks = get_active_forks(repoName, _user.github_access_token)
+
+        for fork in forkNames:
+            forkCommits[fork] = fetch_commit_list(repoName, fork)
+            forkSentences.append(fetch_commit_list(repoName, fork))
+
+        # extract common keywords between all selected forks
+        common_keywords = Rake.extract_keywords_from_sentences(forkSentences)
+
+        return {
+            "keywords" : common_keywords,
+        }
