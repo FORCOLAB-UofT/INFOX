98afce4318ea3feb4591141e1d2d5a4794dce3e3
John Perry <johnperry99@users.noreply.github.com>
2022-02-21 10:41:15 -0500

    Fixed fork comparison backend

--- app/api/ForkComparison.py
+++ app/api/ForkComparison.py
@@ -5,9 +5,15 @@ import json
 from flask import request
 import requests
 from ..models import User, ProjectFork
-from ..analyse.compare_changes_crawler import fetch_commit_list
+from ..analyse.compare_changes_crawler import fetch_commit_list, fetch_diff_code
 from ..analyse.analyser import get_active_forks
 from rake_nltk import Rake
+
+programming_languages = ['html', 'js', 'json', 'py', 'php', 'css','md', 'babel', 'yml']
+common_programming_words = ['if', 'else','for', 'return', 'and', 'or']
+stop_words = programming_languages + common_programming_words
+punctuations = ['\n', '{', '}', '.', '/', "(", ")", ":", "=", ">", "<", "=>", "==", "===", "<=", ">=", ";", "|", '||', '&&', '[', ']', "-"]
+rake = Rake(stopwords=stop_words, punctuations=punctuations)
 
 class ForkComparison(Resource):
     def __init__(self, jwt):
@@ -25,22 +31,39 @@ class ForkComparison(Resource):
 
         forks_info = ProjectFork.objects(project_name=repoName)
 
-        # a dictionary where each key (fork name) contains all commit messages of that fork  
-        # forkCommits = dict()
-
-        # a list of all commits for each fork to be analyzed by Rake
-        forkSentences = []
+        key_words = {}
+        common_words = {}
 
         # active_forks = get_active_forks(repoName, _user.github_access_token)
+        active_forks = get_active_forks(repoName, _user.github_access_token)
+        for fork in active_forks:
+            fork_name = fork["full_name"][: -(len(fork["name"]) + 1)]
+            commit_msgs = fetch_commit_list(repoName, fork_name)
+            code_changes = fetch_diff_code(repoName, fork_name)
 
-        for fork in forkNames:
-            # forkCommits[fork] = fetch_commit_list(repoName, fork)
-            forkSentences.append(fetch_commit_list(repoName, fork))
+            sentences = []
+            for msg in commit_msgs:
+                sentences.append(msg['title'])
 
-        # extract common keywords between all selected forks
-        common_keywords = Rake.extract_keywords_from_sentences(forkSentences)
+            for fork_info in code_changes:
+                if fork_info['file_full_name']:
+                    sentences.append(fork_info['file_full_name'])
+                
+                if fork_info['added_code']:
+                    sentences.append(fork_info['added_code'])
+            
+            if sentences:
+                rake.extract_keywords_from_sentences(sentences)
+                key_words[fork_name] = rake.get_ranked_phrases()
+
+        for key, value in key_words.items():
+            for word in value:
+                if word not in common_words:
+                    common_words[word] = [key]
+                else:
+                    common_words[word].append(key)
 
         return {
-            "keywords" : common_keywords,
+            "keywords" : common_words,
             "forks" : forks_info
         }
