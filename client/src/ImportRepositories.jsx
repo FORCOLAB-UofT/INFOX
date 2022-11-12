ce02c214c9dfc1448177ea3555f096b0b605f846
John Melric Espedido <melricespedido@outlook.com>
2022-11-11 22:12:02 -0500

    Fixing case when no public repos found in account

--- client/src/ImportRepositories.jsx
+++ client/src/ImportRepositories.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useCallback, useState } from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import isEmpty from "lodash/isEmpty";
 import { getUserFollowedRepositories, getUserImportRepositories } from "./repository";
 import { LOADING_HEIGHT } from "./common/constants";
@@ -138,85 +138,132 @@ const ImportRepositories = () => {
     }
   }, [importRepositories]);
 
-  return (
-    <Box>
-      {isLoading ? (
-        <Box height={LOADING_HEIGHT}>
-          <Loading />
-        </Box>
-      ) : (
-        <Box width="100%">
-          <Title text="Import Repositories" />
-          <Box paddingLeft="4px">
-            <Box>
-              <SearchAndFilter
-                filters={filtersWithValues}
-                setFilters={(data) => {
-                  setFilters(data);
-                }}
-                setSearch={(data) => {
-                  setSearch(data);
-                }}
-              />
-            </Box>
-            <Box>
-              {filteredRepositories?.map(
-                ({ repo, language, description, updated, timesForked }) => (
-                  <ImportRepositoryCard
-                    key={repo}
-                    name={repo}
-                    language={language}
-                    description={description}
-                    timesForked={timesForked}
-                    onFollow={(data) => {
-                      setFollowMsg(data.msg);
-                      setOpenSnackbar(true);
-                      setFollowedRepositories([...followedRepositories, data.repo]);
-                    }}
-                    onRemoveRepo={(value) => {
-                      setFollowedRepositories(
-                        followedRepositories.filter(
-                          (repo) => repo.repo !== value
-                        )
-                      );
-                    }}
-                    followedRepos={followedRepositories}
-                  />
-                )
-              )}
+
+  if (!isEmpty(importRepositories)) {
+    return (
+      <Box>
+        {isLoading ? (
+          <Box height={LOADING_HEIGHT}>
+            <Loading />
+          </Box>
+        ) : (
+          <Box width="100%">
+            <Title text="Import Repositories" />
+            <Box paddingLeft="4px">
+              <Box>
+                <SearchAndFilter
+                  filters={filtersWithValues}
+                  setFilters={(data) => {
+                    setFilters(data);
+                  }}
+                  setSearch={(data) => {
+                    setSearch(data);
+                  }}
+                />
+              </Box>
+              <Box>
+                {filteredRepositories?.map(
+                  ({ repo, language, description, updated, timesForked }) => (
+                    <ImportRepositoryCard
+                      key={repo}
+                      name={repo}
+                      language={language}
+                      description={description}
+                      timesForked={timesForked}
+                      onFollow={(data) => {
+                        setFollowMsg(data.msg);
+                        setOpenSnackbar(true);
+                        setFollowedRepositories([...followedRepositories, data.repo]);
+                      }}
+                      onRemoveRepo={(value) => {
+                        setFollowedRepositories(
+                          followedRepositories.filter(
+                            (repo) => repo.repo !== value
+                          )
+                        );
+                      }}
+                      followedRepos={followedRepositories}
+                    />
+                  )
+                )}
+              </Box>
             </Box>
           </Box>
-        </Box>
-      )}
-      <Stack spacing={2} sx={{ width: "100%" }}>
-        <Snackbar
-          open={openSnackbar}
-          autoHideDuration={6000}
-          onClose={(event, reason) => {
-            if (reason === "clickaway") {
-              return;
-            }
-
-            setOpenSnackbar(false);
-          }}
-        >
-          <Alert
+        )}
+        <Stack spacing={2} sx={{ width: "100%" }}>
+          <Snackbar
+            open={openSnackbar}
+            autoHideDuration={6000}
             onClose={(event, reason) => {
               if (reason === "clickaway") {
                 return;
               }
-
+  
               setOpenSnackbar(false);
             }}
-            severity="success"
-            sx={{ width: "100%" }}
           >
-            {followMsg}
-          </Alert>
-        </Snackbar>
-      </Stack>
-    </Box>
-  );
+            <Alert
+              onClose={(event, reason) => {
+                if (reason === "clickaway") {
+                  return;
+                }
+  
+                setOpenSnackbar(false);
+              }}
+              severity="success"
+              sx={{ width: "100%" }}
+            >
+              {followMsg}
+            </Alert>
+          </Snackbar>
+        </Stack>
+      </Box>
+    );
+  }
+
+  else {
+    return (
+      <Box>
+        {isLoading ? (
+          <Box height={LOADING_HEIGHT}>
+            <Loading />
+          </Box>
+        ) : (
+          <Box width="100%">
+            <Typography variant="h3" textAlign="center">Import Repositories</Typography>
+            <Typography variant="h5" textAlign="center">No public repositories found in account.</Typography>
+          </Box>
+        )}
+        <Stack spacing={2} sx={{ width: "100%" }}>
+          <Snackbar
+            open={openSnackbar}
+            autoHideDuration={6000}
+            onClose={(event, reason) => {
+              if (reason === "clickaway") {
+                return;
+              }
+  
+              setOpenSnackbar(false);
+            }}
+          >
+            <Alert
+              onClose={(event, reason) => {
+                if (reason === "clickaway") {
+                  return;
+                }
+  
+                setOpenSnackbar(false);
+              }}
+              severity="success"
+              sx={{ width: "100%" }}
+            >
+              {followMsg}
+            </Alert>
+          </Snackbar>
+        </Stack>
+      </Box>
+    );
+  }
 };
 
 export default ImportRepositories;
